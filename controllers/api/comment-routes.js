abf9e6fed87412d88177c444275c2c1586db2900
Manny <mannysangha020@gmail.com>
2022-11-16 10:17:46 -0800

    finished work on router.get('/')

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,5 +3,11 @@ const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req,res) => {
-    
-})
+    Comment.findAll({})
+     .then(dbCommentData => res.json(dbCommentData))
+     .catch(err => {
+         console.log(err)
+         res.status(500).json(err);
+     });
+});
+
