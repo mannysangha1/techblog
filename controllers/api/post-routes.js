cea550cfd4e094be849e662a92a2a550b11f2588
Manny <mannysangha020@gmail.com>
2022-11-17 13:12:01 -0800

    finished added coding on router.get('/:id')

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,6 +4,7 @@ const sequelize = require('../..config/connection');
 const withAuth = require('../../utils/auth.js');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
+const command = require('nodemon/lib/config/command');
 
 // get all users
 router.get('/', (req, res) => {
@@ -39,5 +40,37 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    post.findAll({})
-})
+    post.findAll({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            // Comment model here -- attached username to comment
+            {
+                model: Comment,
+                attributes: ['id', 'title', 'created_at', 'post_content'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'twitter', 'github']
+                }
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id'})
+                return;
+            }
+            res.json(dbPostData);
+    })
+        .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+     });
+});
