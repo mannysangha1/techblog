ad34b33767c49f338322c52bbc3f8920460d2659
Manny <mannysangha020@gmail.com>
2022-11-17 11:40:56 -0800

    started work on router.get('/')

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const sequelize = require('../..config/connection');
+const withAuth = require('../../utils/auth.js');
+
+// get all users
+router.get('/', (req, res) => {
+    
+})
