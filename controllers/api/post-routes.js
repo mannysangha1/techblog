c73d7b7b63f59e9b1de836a17fb71bc68c8c617e
Manny <mannysangha020@gmail.com>
2022-11-18 09:10:01 -0800

    finished adding code to router.put('/:id')

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -91,5 +91,24 @@ router.post('/', withAuth, (res, req) => {
 });
 
 router.put('/:id', withAuth, (req, res) => {
-    
-})
+    Post.update({
+        title: req.body.title,
+        post_content: req.body.post_content
+       },
+       {
+           where: {
+               id: req.params
+           }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No Post found with this id'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
