28a009ca25ad39a0289fe9c79179bca35475b81b
Manny <mannysangha020@gmail.com>
2022-11-23 21:27:01 -0800

    began work on router.get('/:id')

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,3 +2,20 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// Get /api/users
+router.get('/', (req, res) => {
+    // Access our User model and run .findAll() method
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    
+})
