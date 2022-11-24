5856b904aab9fafe70ddb606fc721351d0a6c751
Manny <mannysangha020@gmail.com>
2022-11-24 09:46:41 -0800

    moved to a different file

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            // use the ID from the session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'linkedin', 'github']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username' ]
+            }
+        ]
+    })
+})
--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
-
-router.get('/', withAuth, (req, res) => {
-    
-})
