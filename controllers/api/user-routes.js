1a7cd6ee96b01f720a1dcf384acbe4c1d149d14f
Manny <mannysangha020@gmail.com>
2022-11-23 22:19:11 -0800

    finished work on router.get('/:id')

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -17,5 +17,37 @@ router.get('/', (req, res) => {
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
-    
-})
+    User.findAll({
+        attributes: ['id', 'title', 'post_content', 'created_at'],
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_content', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+
+    })
+      .then(dbUserData => {
+          if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id'});
+              return;
+          }
+          res.json(dbUserData);
+      })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
