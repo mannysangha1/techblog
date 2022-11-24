e891e62783f9d57e3638d6fa0a847337e0b6654b
Manny <mannysangha020@gmail.com>
2022-11-24 15:27:13 -0800

    finished work on router.get('/login')

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,9 +41,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.loggedIn){
-        if (req.session.loggedIn) {
-            
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return
         }
-    }
-})
+
+    res.render('login');
+});
