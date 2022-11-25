25606a146823cd7c14503bca22c479d6e6105bc4
Manny <mannysangha020@gmail.com>
2022-11-24 16:08:54 -0800

    added consts to index.js

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+const apiRoutes = require('express').Router('./api');
+const homeRoutes = require('./home-routes')
+const dashboardRoutes = require('./dashboard-routes');
+
