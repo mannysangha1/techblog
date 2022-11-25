29e7cd52967f25ecca03358334af1971283619d8
Manny <mannysangha020@gmail.com>
2022-11-25 01:48:22 -0800

    added class User extends Model{}

--- models/User.js
+++ models/User.js
@@ -2,3 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
+// create our User model
+class User extends Model {
+    // set up meyhod to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compare(loginPw, this.password);
+    }
+}
+
+// define table columns and configuaration
