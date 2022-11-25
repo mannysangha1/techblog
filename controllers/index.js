aca21483c3d248c919068806186f1bd80a8ce65f
Manny <mannysangha020@gmail.com>
2022-11-24 16:14:26 -0800

    added router.use to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,3 +4,12 @@ const apiRoutes = require('express').Router('./api');
 const homeRoutes = require('./home-routes')
 const dashboardRoutes = require('./dashboard-routes');
 
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
